3033b85d8e6145fdac9cfeeb825dad26b18d9cc2
Gustavo <gustavo.motta23@gmail.com>
2024-01-28 20:40:40 -0300

    feat: Added docker and hot reload with air

--- database/main.go
+++ database/main.go
@@ -10,7 +10,7 @@ var Session *gorm.DB
 func Connect() (*gorm.DB, error) {
 	var err error
 
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+	dsn := "host=db-dev user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
 	Session, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	return Session, err
