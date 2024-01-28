7285f56f7670cc3201f069b86accef021dbda17b
Gustavo <gustavo.motta23@gmail.com>
2024-01-28 20:47:16 -0300

    feat: Created env file

--- database/main.go
+++ database/main.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,7 +12,8 @@ var Session *gorm.DB
 func Connect() (*gorm.DB, error) {
 	var err error
 
-	dsn := "host=db-dev user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+	dsn := os.Getenv("DATABASE_DSN")
+
 	Session, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	return Session, err
