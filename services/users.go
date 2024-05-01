4b8055c5e8b43e28097e62a3d92a4b89a3299886
Gustavo <gustavo.motta23@gmail.com>
2024-05-01 19:36:12 -0300

    feat: Refactored services

---
+++ services/users.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"gmotta/login/database"
+	"gmotta/login/models"
+	"gmotta/login/schemas"
+
+	"gorm.io/gorm"
+)
+
+func NewUserFromCreateUserData(userData schemas.CreateUserData) models.User {
+	return models.User{
+		Name:      userData.Name,
+		Email:     userData.Email,
+		Password:  userData.Password,
+		BirthDate: userData.BirthDate,
+	}
+}
+
+func CreateUser(userData schemas.CreateUserData) (*gorm.DB, error) {
+	user := NewUserFromCreateUserData(userData)
+
+	result := database.Session.Create(&user)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return result, nil
+}
+
+func GetUsers() ([]models.User, error) {
+	var users []models.User
+	result := database.Session.Find(&users)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return users, nil
+}
